2858bba7f25a9fbdf20963a21dfe5638fee2094d
Denis <whigjet@yandex.ru>
2025-08-27 21:26:01 +0300

    GT pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/your-repo-name/' : '/',
+  base: "/demo-standup/",
   build: {
     outDir: 'dist'
   }
